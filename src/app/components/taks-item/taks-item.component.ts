69f7b0f4a8003c12cb8fa8fe29cf3b51fba1cb3a
KLukas04 <krinkelukas@gmail.com>
2021-12-15 22:26:03 +0100

    Agular Learning Project - Task Tracker

---
+++ src/app/components/taks-item/taks-item.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Task} from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-taks-item',
+  templateUrl: './taks-item.component.html',
+  styleUrls: ['./taks-item.component.css']
+})
+export class TaksItemComponent implements OnInit {
+  @Input() task?: Task
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
+  faTimes = faTimes;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDelete(task: Task){
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task);
+  }
+}
